0c1c6cbd27b1af493128992d314d2ae9aeb6a6b4
Adil Mezghouti <adilmezghouti@gmail.com>
2015-05-24 21:56:34 +0100

    initial commit

---
+++ server.R
@@ -0,0 +1,34 @@
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = input$radioId, border = 'white')
+    
+  })
+  
+  output$textId <- renderText({ 
+    paste(input$textId)
+  })
+  
+  output$buttonId <- renderText({ 
+    paste("Our Demo radio button selected is: ", input$radioId)
+  })
+  
+  output$slider <- renderText({ 
+    paste("Our slider has this value: ", input$bins)
+  })
+  
+})
+
